5f4098972965c888f7afdb6db47a25b08f13c8f6
ajettesla <56906721+ajettesla@users.noreply.github.com>
2024-07-01 14:40:32 +0200

    Update clientmain.cpp

--- clientmain.cpp
+++ clientmain.cpp
@@ -16,7 +16,8 @@
 //github  https://github.com/ajettesla/Assignment1a.git
 
 std::vector<std::string> split(std::string sString,std::string delimiter);
-void gsready(std::string &ip, int port, struct sockaddr_in *ipv4,struct sockaddr_in6 *ipv6, int* ipstatus);
+
+int gsready(std::string &ip, int port,int* ipstatus);
 
 
 std::vector<std::string> nString;
@@ -36,14 +37,12 @@ char* math(std::string string, double a, double b);
 
 
 int main(int argc, char *argv[]){
-
-
-
 std::string delimiter = ":";
 
 std::vector<std::string> outputString = split(argv[1],":"); 
 
 std::string ipString = "";
+
 int port;
 
 if(outputString.size() > 2){
@@ -65,13 +64,11 @@ ipString = outputString[0];
   std::cout << "Host Name is " << ipString << " port " << port << std::endl;
 #endif
 
-struct sockaddr_in6 *ipv6 = new struct sockaddr_in6;
 
-struct sockaddr_in *ipv4 = new struct sockaddr_in;
 
 int *ipstatus = new int;
 
-gsready(ipString ,port, ipv4, ipv6, ipstatus);
+int socketfd = gsready(ipString ,port, ipstatus);
 
 #ifdef DEBUG 
   if(*ipstatus == 1){
@@ -81,135 +78,93 @@ gsready(ipString ,port, ipv4, ipv6, ipstatus);
   }
 #endif
 
-
-if(*ipstatus == 1){
-   int socketfd;
-   socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(socketfd == -1) {
-        std::cerr << "Error in socket creation" << std::endl;
-        return 1;
-    }
-
-   if(connect(socketfd, (struct sockaddr*)ipv4,sizeof(struct sockaddr_in)) == -1){
-    std::cout << "Error in connect" << std::endl;
-    close(socketfd);}
-
     std::string okChar = "OK\n";
-     
-    recvBuffer(socketfd);
-    sendBuffer(socketfd,okChar.c_str());
-    std::string message = recvBuffer(socketfd);
-    std::vector<std::string> inputVector = split(message," ");
-    double a = std::stod(inputVector[1]);
-    double b = std::stod(inputVector[2]);
-    char *result = math(inputVector[0], a, b);
-    sendBuffer(socketfd, result);
-    recvBuffer(socketfd);
-    close(socketfd);
- 
-             }
-else if(*ipstatus == 2){
-
-     int socketfd;
-     
-     socketfd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
-     
-     if(socketfd == -1) {
-        std::cout << "Error in socket creation" << std::endl;
-        return 1;
+    std::string firstmes = recvBuffer(socketfd);
+    if(firstmes == "TEXT TCP 1.0\n\n"){
+        sendBuffer(socketfd,okChar.c_str());
+        std::string message = recvBuffer(socketfd);
+        std::vector<std::string> inputVector = split(message," ");
+        double a = std::stod(inputVector[1]);
+        double b = std::stod(inputVector[2]);
+        char *result = math(inputVector[0], a, b);
+        sendBuffer(socketfd, result);
+        recvBuffer(socketfd);}
+    else{
+        std::cout << "error " << std::endl;
     }
+    close(socketfd); 
     
-    if(connect(socketfd, (struct sockaddr*)ipv6,sizeof(struct sockaddr_in6)) == -1){
-    perror("connect error is " );
-    close(socketfd);
-    return 1;
-    }
-    
-    std::string okChar = "OK\n";
-    recvBuffer(socketfd);
-    sendBuffer(socketfd,okChar.c_str());
-    std::string message = recvBuffer(socketfd);
-    std::vector<std::string> inputVector = split(message," ");
-    double a = std::stod(inputVector[1]);
-    double b = std::stod(inputVector[2]);
-    char *result = math(inputVector[0], a, b);
-    sendBuffer(socketfd, result);
-    recvBuffer(socketfd);
-    close(socketfd);
-                }
-
-
 }
 
 
-void gsready(std::string &ip, int port, sockaddr_in *ipv4, sockaddr_in6 *ipv6,int* ipstatus){
-
+int gsready(std::string &ip, int port,int* ipstatus){
+int socketfd; 
+struct sockaddr_in ipv4;
+struct sockaddr_in6 ipv6;
 struct addrinfo hint, *output, *temp;
 memset(&hint, 0, sizeof(hint));
 hint.ai_family = AF_UNSPEC;
 hint.ai_socktype = SOCK_STREAM;
 int status = getaddrinfo(ip.c_str(), NULL, &hint, &output);
 if(status != 0){
-std::cout << "There is problem in getting getaddrinfo" << std::endl;
-
-}
+std::cout << "There is problem in getting getaddrinfo" << std::endl;}
 
 for(temp=output; temp != NULL;temp->ai_addr){
 
 if(temp->ai_family == AF_INET){
-ipv4->sin_family = AF_INET;
-ipv4->sin_port = htons(port);
-ipv4->sin_addr.s_addr = ((struct sockaddr_in*)temp->ai_addr)->sin_addr.s_addr;
-*ipstatus = 1;
-break;
-                            }
+ipv4.sin_family = AF_INET;
+ipv4.sin_port = htons(port);
+ipv4.sin_addr.s_addr = ((struct sockaddr_in*)temp->ai_addr)->sin_addr.s_addr;
+socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+if(socketfd > 0){
+   if(connect(socketfd, (struct sockaddr*)&ipv4, sizeof(struct sockaddr)) < 0){perror("error with connect");close(socketfd);fflush(stderr);exit(1);}
+    std::cout << "Connected to " << ip << ":" << port << std::endl;
+  *ipstatus = 1;
+   break;
+}}
                                               
 else if(temp->ai_family == AF_INET6){
-ipv6->sin6_family = AF_INET6;
-ipv6->sin6_port = htons(port);
-ipv6->sin6_addr = ((struct sockaddr_in6*)temp->ai_addr)->sin6_addr;
-*ipstatus = 2;
-break;
+ipv6.sin6_family = AF_INET6;
+ipv6.sin6_port = htons(port);
+ipv6.sin6_addr = ((struct sockaddr_in6*)temp->ai_addr)->sin6_addr;
+socketfd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+if(socketfd > 0){
+   if(connect(socketfd, (struct sockaddr*)&ipv6, sizeof(struct sockaddr_in6)) < 0){perror("error with connect");close(socketfd);fflush(stderr);exit(1);}
+   std::cout << "Connected to " << ip << ":" << port << std::endl;
+  *ipstatus = 2;
+   break;
+}}}
 
-}
-
-}
+if(*ipstatus != 1 && *ipstatus != 2){perror("error with socket");close(socketfd);exit(1);}
+  
 freeaddrinfo(output);
+return socketfd;
 }
 
 
 void sendBuffer(int socketfd,const char* mess){
 
 int sent_recv_bytes = send(socketfd, mess, strlen(mess),0);
-
-if(sent_recv_bytes == -1){
-std::cout << "There is error while sending " << std::endl;
-perror("send");}
-
-else{
-std::cout << mess;}
+if(sent_recv_bytes < 0){perror("send");close(socketfd);exit(1);}
+else{std::cout << mess;}
 
 }
 
 std::string recvBuffer(int socketfd){
+
 int bufferSize = 1024;
 char recv_buffer[bufferSize];
+memset(recv_buffer, 0, bufferSize);
 int sent_recv_bytes = recv(socketfd, recv_buffer, bufferSize,0);
-recv_buffer[sent_recv_bytes] = '\0';
 std::string recvBuf(recv_buffer);
-if(sent_recv_bytes == -1){
-std::cout << "There is error while receving data" << std::endl;
-}
-else{
-std::cout << recv_buffer;
-}
+if(sent_recv_bytes < 0){perror("error with recv");close(socketfd);exit(1);}
+else{std::cout << recv_buffer;}
 
 return recvBuf;
 }
 
 
 std::vector<std::string> split(std::string sString,std::string delimiter){
-
 std::vector<std::string> nString;
 std::string temp;
 
